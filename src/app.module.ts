cd2ae0fe1be7f94ee239cbe250dd3f861525781e
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-07-29 15:26:55 +0300

    updated app module, added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './modules/cat/cat.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { config } from './config';
 
 @Module({
-  imports: [CatsModule, MongooseModule.forRoot('mongodb://localhost/test')],
+  imports: [
+    CatsModule,
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: config.HOST,
+      port: config.PORT as number,
+      username: config.USERNAME,
+      password: config.PASSWORD,
+      database: config.DATABASE,
+      models: [],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const config = {
+  HOST: process.env.HOST || '',
+  PORT: process.env.PORT || 3003,
+  USERNAME: process.env.USERNAME || '',
+  PASSWORD: process.env.PASSWORD || '',
+  DATABASE: process.env.DATABASE || '',
+};
