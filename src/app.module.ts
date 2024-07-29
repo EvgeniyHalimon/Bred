224b2a1cc9c9593d4900522a99a861c3b22bb9f4
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-07-29 14:35:13 +0300

    removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './modules/cat/cat.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [CatsModule, MongooseModule.forRoot('mongodb://localhost/test')],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
