ff7daeb23aba5239469e725d85700272b75bab9c
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-07-29 18:20:13 +0300

    updated service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,19 +3,37 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/schemas/user.schema';
 import { IUser } from './interfaces/user.interfaces';
+import { Sequelize } from 'sequelize';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userModel: typeof User) {}
+  constructor(
+    @InjectModel(User) private userModel: typeof User,
+    private sequelize: Sequelize,
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const userAttributes: IUser = {
-      firstName: createUserDto.firstName,
-      lastName: createUserDto.lastName,
-      isActive: createUserDto.isActive,
-    };
-    const createdUser = this.userModel.create(userAttributes);
-    return createdUser;
+    try {
+      const result = await this.sequelize.transaction(async t => {
+        const transactionHost = { transaction: t };
+
+        const userAttributes: IUser = {
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+          isActive: createUserDto.isActive,
+        };
+        const createdUser = await this.userModel.create(
+          userAttributes,
+          transactionHost,
+        );
+        return createdUser;
+      });
+
+      return result;
+    } catch (err) {
+      console.log('🚀 ~ UsersService ~ create ~ err:', err);
+      throw err;
+    }
   }
 
   async findAll(): Promise<User[]> {
