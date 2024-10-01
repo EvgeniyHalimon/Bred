596415f766de9ffa00bb1e3687bb8838b32b12b7
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-10-01 12:59:11 +0300

    updates

--- src/articles/dto/index.ts
+++ src/articles/dto/index.ts
@@ -1,5 +1,5 @@
 export * from './create-article.dto';
 export * from './patch-article.dto';
 export * from './get-all-article.dto';
-export * from './delete-article-response.dto';
+export * from './delete-article.dto';
 export * from './article-presenter';
